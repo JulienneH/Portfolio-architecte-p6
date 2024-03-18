c775d2f9b205ea3a5a06c38a8fb80840a0fd6976
Julienne H <juliennehattabi@gmail.com>
2024-03-18 11:11:02 +0100

    chore : layout of works

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,5 +1,8 @@
+//fonction pour récupérer les works depuis le backend
+// et pour retourner la réponse sous format json
+
 async function getWorks() {
-    const response = await fetch("http://localhost:5678/api/works");
+    const response = await fetch("http://localhost:5678/api/works"); //GET par défaut
     const works = await response.json();
     return works;
 }
@@ -7,32 +10,37 @@ getWorks();
 
 //affichage des travaux dans le DOM 
 async function displayWorks() {
+
     const displayWorks = document.createElement("div");
 
     // obtenir les travaux à afficher en appelant la fonction getWorks
     const arrayworks = await getWorks();
 
+    //pour chaque travaux je créé une balise figure
+
     arrayworks.forEach((work) => {
-        const workElement = document.createElement("div");
+        const workElement = document.createElement("figure");
+        displayWorks.classList.add("gallery");
+
 
         // Créer un élément img pour chaque URL d'image 
         const imageElement = document.createElement("img");
         imageElement.src = work.imageUrl;
 
-        // Ajout de l'élément image au conteneur de nos travaux 
+        // Ajout de l'élément image dans <figure> 
         workElement.appendChild(imageElement);
 
-        // Ajout du titre 
-        const titleElement = document.createElement("h3");
+        // Ajout du titre dans <figure>
+        const titleElement = document.createElement("figcaption");
         titleElement.innerText = work.title;
         workElement.appendChild(titleElement);
 
-        // Ajout des travaux à l'élément d'affichage 
+        // Ajout de <figure> dans displayWorks donc ajout des works dans le conteneur
         displayWorks.appendChild(workElement);
     });
 
-    // Ajout de l'élément d'affichage au corps de la page
-    document.body.appendChild(displayWorks);
+    // Ajout des works dans la section avec l'id portfolio dans le DOM 
+    document.getElementById("portfolio").appendChild(displayWorks);
 }
 
 displayWorks();
